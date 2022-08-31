00845230351f11189ef99122dd90988709efe014
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2022-08-31 10:02:47 +0100

    Added flutter logo svg

---
+++ lib/pages/raw_svg_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class RawSvgPage extends StatefulWidget {
+  const RawSvgPage({Key? key}) : super(key: key);
+
+  @override
+  State<RawSvgPage> createState() => _RawSvgPageState();
+}
+
+class _RawSvgPageState extends State<RawSvgPage> {
+//   final String rawSvg = '''<svg viewBox="...">...</svg>''';
+//   final DrawableRoot svgRoot = await svg.fromSvgString(rawSvg, rawSvg);
+//
+// // If you only want the final Picture output, just use
+//   final Picture picture = svgRoot.toPicture();
+//
+// // Otherwise, if you want to draw it to a canvas:
+// // Optional, but probably normally desirable: scale the canvas dimensions to
+// // the SVG's viewbox
+//   svgRoot.scaleCanvasToViewBox(canvas);
+//
+// // Optional, but probably normally desireable: ensure the SVG isn't rendered
+// // outside of the viewbox bounds
+//   svgRoot.clipCanvasToViewBox(canvas);
+//   svgRoot.draw(canvas, size);
+//
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
